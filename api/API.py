62f18e8bcc9ad94e8dc7a9b23de2c662687ae144
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-08-05 14:00:21 -0400

    Feat: Add new POST, accomodate new supply feature and day calculator

--- api/API.py
+++ api/API.py
@@ -22,23 +22,34 @@ async def get_water_from_weight(weight: int):
 @api.get("/supply")
 async def get_supply():
     """Returns the amount of water in liters stored in the DB."""
-    water_supply = await AsyncNumberTools.get_total_water_supply()
-    return {"supply": f"{water_supply}"}
+    water_supply, weight, days = await AsyncNumberTools.get_totals()
+    return {"supply": f"{water_supply}", "weight": f"{weight}", "days": f"{days}"}
 
 
-@api.post("/edit/{operation}/{liters}")
-async def update_water(operation: str, liters: int):
+@api.post("/editliters/{operation}/{liters}")
+async def edit_water(operation: str, liters: int):
     """Updates the water in liters in the DB.
     Provide the operation (+ or - symbol) and the amount you want to change by"""
-    # curl -X POST 127.0.0.1:8000/edit/+/500
-    water_supply = await AsyncNumberTools.get_total_water_supply()
+    # curl -X POST 127.0.0.1:8000/editwater/+/500
+    water_supply = await AsyncNumberTools.get_totals()
     if operation == "+":
-        liters = liters + water_supply
+        liters = liters + water_supply[0]
     elif operation == "-":
-        liters = water_supply - liters
+        liters = water_supply[0] - liters
     result = await AsyncNumberTools.update_water_amount(liters)
     return {"result": f"{result}"}
 
 
-if __name__ == "__main__":
-    uvicorn.run(api, host="127.0.0.1", port=8000, reload=True)
+@api.post("/editweight/{operation}/{weight}")
+async def edit_weight(operation: str, weight: int):
+    """Updates the weight in pounds in the DB.
+    Provide the operation (+ or - symbol) and the amount you want to change by"""
+    # curl -X POST 127.0.0.1:8000/editweight/+/500
+    totals = await AsyncNumberTools.get_totals()
+    if operation == "+":
+        weight = weight + totals[1]
+    elif operation == "-":
+        weight = totals[1] - weight
+    result = await AsyncNumberTools.update_weight(weight)
+    return {"result": f"{result}"}
+
