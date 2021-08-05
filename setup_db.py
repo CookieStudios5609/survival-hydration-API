3211b66fcd31a3441fe6a5d556402cfeb06f5110
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-08-05 14:01:53 -0400

    Feat BREAKING: Change DB layout
    Changes DB layout to now include stored weight and survival days for persistence. Requires entirely remaking the DB.

--- setup_db.py
+++ setup_db.py
@@ -6,13 +6,13 @@ def make_db():
     try:
         conn = sqlite3.connect("api/water.db")
         cursor = conn.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS watersupply (liters INTEGER PRIMARY KEY);")
+        cursor.execute("CREATE TABLE IF NOT EXISTS watersupply (liters INTEGER, weight INTEGER, days INTEGER);")
         conn.commit()
-        cursor.execute("INSERT INTO watersupply(liters) VALUES(1)")
+        cursor.execute("INSERT INTO watersupply(liters, weight, days) VALUES(1, 100, 0)")
         conn.commit()
         cursor.execute("SELECT * FROM watersupply")
         p = cursor.fetchone()
-        print(p[0])
+        print(f"{p[0]} liters, {p[1]} pounds, {p[2]} days")
     except Error as e:
         print(e)
     finally:
