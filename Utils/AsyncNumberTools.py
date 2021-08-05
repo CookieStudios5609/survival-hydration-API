4a4b3747aa88f357885635ca59e49e0b0ab9f392
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-08-05 13:59:08 -0400

    Feat: Add more verbose totals, day calculation
    this commit replaces get_water_supply with get_totals, which now grabs weight, water, and days all stored in the DB. To make updating the DB easier, an update_weight function was made, and to get the days, calculate_days was made. These all need polishing.

--- Utils/AsyncNumberTools.py
+++ Utils/AsyncNumberTools.py
@@ -15,12 +15,20 @@ async def ounces_to_liters(ounces: float) -> float:
     return round(number=ounces / 33.814, ndigits=2)
 
 
-async def get_total_water_supply() -> int:
-    """Returns the total water supply in liters from an sqlite3 DB"""
+# changing this function and the DB structure increased the chance of division by 0 greatly. Work on this.
+# also it looks ugly now. There is a simpler way. Take out time to find it.
+# instead of calculating the days each time, maybe start updating the days in the DB automatically,
+# maybe on a time interval? Also, why convert everything to float at some point to convert back to int? plsfix
+async def get_totals() -> tuple:
+    """Returns the total water supply in liters, stored weight in pounds, and time in days one could survive
+     from an sqlite3 DB"""
     async with aiosqlite.connect("api/water.db") as db:
-        cursor = await db.execute("SELECT * FROM watersupply")
-        liters = await cursor.fetchone()
-        return int(liters[0])
+        cursor = await db.execute("SELECT liters, weight FROM watersupply")
+        liters_weight = await cursor.fetchone()
+        water_ounces = await get_required_water_intake(weight=liters_weight[1])
+        liter = await ounces_to_liters(ounces=water_ounces)
+        days = await calculate_days(int(liter), liters_weight[0])
+        return liters_weight[0], liters_weight[1], int(days)
 
 
 async def update_water_amount(new_liters: int) -> str:
@@ -32,3 +40,22 @@ async def update_water_amount(new_liters: int) -> str:
         except aiosqlite.Error as e:
             new_liters = e
         return str(new_liters)
+
+
+async def update_weight(new_weight: int) -> str:
+    """Allows one to change the weight in pounds stored in the DB"""
+    async with aiosqlite.connect("api/water.db") as db:
+        try:
+            await db.execute("UPDATE watersupply SET weight = ?", (new_weight,))
+            await db.commit()
+        except aiosqlite.Error as e:
+            new_weight = e
+        return str(new_weight)
+
+
+# sometimes causes div/0 errors, find a suitable solution
+async def calculate_days(water_required: int, water_total: int) -> int:
+    days = water_total/water_required
+    async with aiosqlite.connect("api/water.db") as db:
+        await db.execute("UPDATE watersupply SET days = ?", (days,))
+    return int(days)
