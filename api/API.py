8dd360e37a0a73cb322835fde79fd0b4685f45fa
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-08-16 07:40:56 -0400

    Feat: Add CORS
    This commit adds CORS middleware, so the HTML site can access the API without error.

--- api/API.py
+++ api/API.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
 from Utils import AsyncNumberTools
-
+from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
+
+origins = ["*"]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @api.get("/")
