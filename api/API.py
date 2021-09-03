818fc5c8447f8931b9fdc58153d131b3e3ba9a77
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-09-03 05:39:10 -0400

    Feat: Improve POST endpoints
    This commit replaces the current POST endpoints with ones that accept a request body, rather than havng information like the increment in a URL.

--- api/API.py
+++ api/API.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from Utils import AsyncNumberTools
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 api = FastAPI()
 
@@ -14,6 +15,11 @@ api.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+class EditRequest(BaseModel):
+    type: str
+    increment: int
 
 
 @api.get("/")
@@ -34,6 +40,33 @@ async def get_supply():
     """Returns the amount of water in liters stored in the DB."""
     water_supply, weight, days = await AsyncNumberTools.get_totals()
     return {"supply": f"{water_supply}", "weight": f"{weight}", "days": f"{days}"}
+
+
+@api.post("/edit")
+async def edit_stored_stats(request: EditRequest):
+    """Send a POST here with the appropriate body to edit the saved weight/water in the DB.
+    Positive/negative integers are accepted in the body."""
+# curl -X POST -H "Content-Type:application/json" -d "{\"type\":\"weight\",\"increment\":\"-15\"}" http://127.0.0.1:8000/edit
+# curl -X POST -H "Content-Type:application/json" -d "{\"type\":\"liter\",\"increment\":\"5\"}" http://127.0.0.1:8000/edit
+    totals = await AsyncNumberTools.get_totals()
+    # try/except is useless here, as FastAPI returns its own errors before the except block starts. Cool!
+    # Remove this/read docs
+    if request.type == "liter":
+        new_liters = request.increment + totals[0]
+        try:
+            response = await AsyncNumberTools.update_water_amount(new_liters)
+            return {"status": "ok", "new value": f"{response}"}
+        except Exception as e:
+            return {"status": f"Error: {e}"}
+    elif request.type == "weight":
+        new_weight = request.increment + totals[1]
+        try:
+            response = await AsyncNumberTools.update_weight(new_weight)
+            return {"status": "ok", "new value": f"{response}"}
+        except Exception as e:
+            return {"status": f"Error: {e}"}
+    else:
+        raise HTTPException(status_code=400, detail=f"Invalid input: Editing {request.type} is not supported.")
 
 
 @api.post("/editliters/{operation}/{liters}")
